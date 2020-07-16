151a9d389f71bf5d1cf996eb5f97fdaa6167e571
Lucas <ldanezine@gmail.com>
2020-07-16 01:26:52 -0300

    Implementada a classe circle

---
+++ ExcAbstractOne/Entities/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExcAbstractOne.Entities.Enums;
+
+namespace ExcAbstractOne.Entities
+{
+    class Circle : Shape
+    {
+        public double Rad { get; set; }
+
+        public Circle(double rad, Color color) : base (color)
+        {
+            Rad = rad;
+        }
+
+        public override double Area()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
