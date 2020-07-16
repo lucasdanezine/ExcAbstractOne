882f0881b6e43f0269b817f72b2bb1e0e8d3a978
Lucas <ldanezine@gmail.com>
2020-07-16 01:09:21 -0300

    Adicionar arquivos de projeto.

---
+++ ExcAbstractOne/Entities/Enums/Color.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcAbstractOne.Entities.Enums
+{
+    enum Color : int
+    {
+        Black = 0,
+        Blue = 1,
+        Red = 2
+
+    }
+}
